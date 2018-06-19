c002ff1eeb802d3048c7ae98337ebf01e8c06840
unknown <ipetrovbg@gmail.com>
2018-06-19 08:54:46 +0300

    update

--- src/app/store/effects/user.effects.ts
+++ src/app/store/effects/user.effects.ts
@@ -1,11 +1,15 @@
+import { UserState } from './../states/user.state';
 import { catchError } from 'rxjs/internal/operators';
 import { Injectable } from '@angular/core';
 import { Actions } from '@ngrx/effects';
 import { Effect } from '@ngrx/effects';
 import { TokenAuthentication, Update, UserActionTypes } from '../actions/user.actions';
-import 'rxjs-compat/add/operator/switchMap';
 import { AuthService } from '../../authentication/auth/auth.service';
 import { Router } from '@angular/router';
+import 'rxjs-compat/add/operator/switchMap';
+import 'rxjs-compat/add/operator/catch';
+import 'rxjs/add/observable/of';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class UserEffects {
@@ -20,17 +24,16 @@ export class UserEffects {
     .map((response: any) => {
       if (response.success) {
         localStorage.setItem('token', response.token);
-        this.router.navigate(['/dashboard']);
 
-        return new Update({
+        return new Update(new UserState({
           id: response.user.id,
           token: response.token,
           email: response.user.email,
           name: response.user.name,
           password: ''
-        });
+        }));
       } else {
-        return new Update({ name: '', email: '', password: '', token: '', id: null });
+        return new Update(new UserState());
       }
-    });
+    }).catch(() => Observable.of(new Update(new UserState())));
 }
--- src/app/store/states/user.state.ts
+++ src/app/store/states/user.state.ts
@@ -1,7 +1,14 @@
-export interface UserState {
+export class UserState {
   email: string;
   password: string;
   token: string;
   name: string;
   id: number;
+  constructor({ email, password, token, name, id } = { email: '', password: '', token: '', name: '', id: null }) {
+    this.email    = email || '';
+    this.password = password || '';
+    this.token    = token || '';
+    this.name     = name || '';
+    this.id       = id || null;
+  }
 }
